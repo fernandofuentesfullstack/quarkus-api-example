13897e0072539f26ecb0f80142fed7870cbefd74
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2021-03-02 19:29:44 +0100

    Added greetingService with test

--- src/test/java/org/acme/getting/started/GreetingResourceTest.java
+++ src/test/java/org/acme/getting/started/GreetingResourceTest.java
@@ -3,14 +3,25 @@ package org.acme.getting.started;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given().pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then().statusCode(200)
+                .body(is("hello " + uuid));
+    }
+
+    @Test
+    void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
